5ab88d69d46385a0cfd72edd9cee455fc67c1ee0
Fernando Orozco <forozcomercado@gmail.com>
2022-12-29 11:31:22 -0500

    ref: add missing angular material import

--- src/app/shared/modules/angular-material.module.ts
+++ src/app/shared/modules/angular-material.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 const angularMaterialModules = [
-  MatToolbarModule, MatIconModule
-]
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+];
 
 @NgModule({
   imports: [...angularMaterialModules],
-  exports: [...angularMaterialModules]
+  exports: [...angularMaterialModules],
 })
 export class AngularModules {}
