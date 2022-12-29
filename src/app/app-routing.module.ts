2929fbd2d8171cd4e6def4b5024e0f5380675477
Fernando Orozco <forozcomercado@gmail.com>
2022-12-29 08:49:34 -0500

    feat: routing set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentDetailsComponent } from './home/students-list/student-details/student-details.component';
+import { StudentsListComponent } from './home/students-list/students-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { 
+        path: 'students', 
+        component: StudentsListComponent,
+        children: [
+          {path: ':id', component: StudentDetailsComponent}
+        ]
+      },
+      
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
